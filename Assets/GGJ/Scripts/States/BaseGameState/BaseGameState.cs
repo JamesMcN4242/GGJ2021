55554eb6e1f6c06422ab01540016f1b51afe4efa
James McNeil <jamesmcneil42@hotmail.co.uk>
2021-01-29 23:34:55 +0000

    Tied the movement system to the player object

--- Assets/GGJ/Scripts/States/BaseGameState/BaseGameState.cs
+++ Assets/GGJ/Scripts/States/BaseGameState/BaseGameState.cs
@@ -7,32 +7,33 @@ using Photon.Realtime;
 public class BaseGameState : FlowStateBase
 {
     private KeyCodeSet m_inputKeys;
-    private GameObject m_player;
-    private Camera m_playerCamera;
+    private Transform m_player;
+    private Transform m_playerCamera;
     private PlayerData m_localPlayerData;
+
     private Vector3 m_cameraRotation;
 
-    private bool Connected => m_localPlayerData != null;
+    private bool Connected => m_player != null;
 
     public BaseGameState(GameObject player, Camera playerCamera)
     {
-        m_player = player;
-        m_playerCamera = playerCamera;
+        m_player = player.transform;
+        m_playerCamera = playerCamera.transform;
         m_inputKeys = Resources.Load<InputKeys>("InputKeys").m_keyCodes;
 
-        m_cameraRotation = Camera.main.transform.eulerAngles;
+        m_cameraRotation = m_playerCamera.eulerAngles;
+
+        //TODO: Decide if seeker or hider
+        m_localPlayerData = Resources.Load<PlayerData>("PlayerData/HiderData");
     }
     
     protected override void UpdateActiveState()
     {
         if (Connected == false) return;
 
-        //Todo: Update to our local device's cube object
-        Transform playerTransform = Camera.main.transform;
-
         Vector2 input = PlayerMovement.GetPlayerMovement(m_inputKeys);
-        PlayerMovement.MovePlayer(playerTransform, Camera.main.transform, input, m_localPlayerData.m_playerSpeed, Time.deltaTime);
-        CameraSystem.UpdateCameraRotation(Camera.main.transform, ref m_cameraRotation);
+        PlayerMovement.MovePlayer(m_player, m_playerCamera, input, m_localPlayerData.m_playerSpeed, Time.deltaTime);
+        CameraSystem.UpdateCameraRotation(m_playerCamera, ref m_cameraRotation);
     }
 
     #region Photon
