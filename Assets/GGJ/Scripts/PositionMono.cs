b1dad51a3c689925e91efaaf3c56bcb701052d37
Jack Davidson <jakd36@gmail.com>
2021-01-30 15:25:12 +0000

    tried further lag compensation

--- Assets/GGJ/Scripts/PositionMono.cs
+++ Assets/GGJ/Scripts/PositionMono.cs
@@ -1,10 +1,21 @@
-﻿using Photon.Pun;
+﻿using System;
+using Photon.Pun;
 using UnityEngine;
 
 public class PositionMono : MonoBehaviour, IPunObservable
 {
     public Vector3 m_velocity;
-    
+    public Vector3 m_start;
+    public Vector3 m_end;
+    public float m_currentTime = 0;
+    public float m_lag;
+
+    public void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(m_start, m_end, m_currentTime / m_lag);
+        m_currentTime += Time.deltaTime;
+    }
+
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
         if (stream.IsWriting)
@@ -17,8 +28,10 @@ public class PositionMono : MonoBehaviour, IPunObservable
             transform.position = (Vector3)stream.ReceiveNext();
             m_velocity = (Vector3) stream.ReceiveNext();
 
-            float lag = Mathf.Abs((float) (PhotonNetwork.Time - info.SentServerTime));
-            transform.position += m_velocity * lag;
+            m_start = m_end;
+            m_lag = Mathf.Abs((float) (PhotonNetwork.Time - info.SentServerTime));
+            m_end += m_velocity * m_lag;
+            m_currentTime = 0;
         }
     }
 }
