bf533b9c6e71601532f7d53cdd5c1f12d947a805
James McNeil <jamesmcneil42@hotmail.co.uk>
2021-01-31 13:39:05 +0000

    Fix commit of missing UI files

---
+++ Assets/GGJ/Scripts/States/BaseGameState/BaseGameUI.cs
@@ -0,0 +1,26 @@
+using PersonalFramework;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BaseGameUI : UIStateBase
+{
+    private TextMeshProUGUI m_powerUpSeconds = null;
+    private Image m_powerUpBacking = null;
+
+    protected override void OnAwake()
+    {
+        m_powerUpSeconds = gameObject.GetComponentFromChild<TextMeshProUGUI>("PowerUpTimer");
+        m_powerUpBacking = gameObject.GetComponentFromChild<Image>("PowerUpFill");
+    }
+
+    public void UpdatePowerUpBacking(float timeRemaining, float maxPowerUpTime)
+    {
+        float fill = timeRemaining / maxPowerUpTime;
+        if(fill != m_powerUpBacking.fillAmount)
+        {
+            m_powerUpBacking.fillAmount = fill;
+            m_powerUpSeconds.text = timeRemaining > 0.3f ? Mathf.CeilToInt(timeRemaining).ToString() : string.Empty;
+        }
+    }
+}
